b8684de46cba7ecc6164c3ac16da482b056141ce
kimskovhusandersen <kimskovhusandersen@gmail.com>
2019-09-02 16:16:43 +0200

    Updated functions based on feedback

--- this/this.js
+++ this/this.js
@@ -1,20 +1,19 @@
 // Exercise 1
-function Square(w_and_h) {
-    this.width = w_and_h;
-    this.height = w_and_h;
-}
-
-Square.prototype.getArea = function() {
-    return this.width * this.height;
-};
-
 function Rectangle(w, h) {
     this.width = w;
     this.height = h;
 }
 
-Rectangle.prototype = Object.create(Square.prototype);
-Rectangle.prototype.constructor = Rectangle;
+Rectangle.prototype.getArea = function() {
+    return this.width * this.height;
+};
+function Square(w_and_h) {
+    this.width = w_and_h;
+    this.height = w_and_h;
+}
+
+Square.prototype = Object.create(Rectangle.prototype);
+Square.prototype.constructor = Square;
 
 var sq = new Square(4);
 var rect = new Rectangle(4, 5);
@@ -28,16 +27,14 @@ console.log("Rectangle: " + rect.getArea()); // 20
 // create a function that takes a string as argument and returns a string. THe strings uppercase characters needs to be lowercase and vice versa.
 // you should use String.toUpperCase and String.toLowerCase
 function invertCase(str) {
-    return typeof str == "string"
-        ? str
-            .split("")
-            .map(function(letter) {
-                return letter == letter.toUpperCase()
-                    ? letter.toLowerCase()
-                    : letter.toUpperCase();
-            })
-            .join("")
-        : false;
+    return str
+        .split("")
+        .map(function(letter) {
+            return letter == letter.toUpperCase()
+                ? letter.toLowerCase()
+                : letter.toUpperCase();
+        })
+        .join("");
 }
 
 console.log(invertCase("Hello NEW Potato!")); // hELLO new pOTATO!
