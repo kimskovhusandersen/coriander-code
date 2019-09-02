044131b49961d2c96843ca4aabf1ad8226df2297
kimskovhusandersen <kimskovhusandersen@gmail.com>
2019-09-02 13:01:15 +0200

    Added dom.js exercises

---
+++ dom/dom.js
@@ -0,0 +1,50 @@
+// Exercise 1:
+
+function changeStyles(selector) {
+    var elements = document.querySelectorAll(selector);
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.fontWeight = "bold";
+        elements[i].style.fontStyle = "italic";
+        elements[i].style.textDecoration = "underline";
+    }
+}
+
+// Exercise 2:
+
+function classesAsArray(className) {
+    return Array.from(document.getElementsByClassName(className));
+}
+
+// Exercise 3:
+function insertElement() {
+    var element = document.createElement("p");
+    element.innerText = "AWESOME";
+    element.style.position = "fixed";
+    element.style.top = "100px";
+    element.style.left = "20px";
+    element.style.fontSize = "200px";
+    element.style.zIndex = "2147483647";
+    document.body.appendChild(element);
+}
+
+// Bonus Exercise:
+function classesAsArray2(className) {
+    var root = document.documentElement;
+    var classesArray = [];
+    keepSearching(root);
+    function keepSearching(root) {
+        if (Object.keys(root.children).length > 0) {
+            for (var i = 0; i < root.children.length; i++) {
+                if (
+                    (" " + root.children[i].className + " ").indexOf(
+                        " " + className + " "
+                    ) > -1
+                ) {
+                    classesArray.push(root.children[i]);
+                }
+                keepSearching(root.children[i]);
+            }
+        }
+    }
+    return classesArray;
+}
